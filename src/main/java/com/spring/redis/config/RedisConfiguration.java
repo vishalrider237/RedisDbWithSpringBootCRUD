1d6835c96750897c2acca7e1b2f24a8a9a5f4e63
Vishal Pandey <vishalpandey10022000@gmail.com>
2024-06-13 23:51:18 +0530

    added commit

--- src/main/java/com/spring/redis/config/RedisConfiguration.java
+++ src/main/java/com/spring/redis/config/RedisConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @org.springframework.context.annotation.Configuration
-public class Configuration {
+public class RedisConfiguration {
 
     @Bean
     public RedisTemplate<String,Object>redisTemplate(){
